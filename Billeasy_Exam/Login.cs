1392a7e2e57e6689bcdf0db70539da4f002e2914
SnigdhaPatwatkar <smpatwatkar2000@gmail.com>
2024-02-02 18:25:06 +0530

    changes

--- Billeasy_Exam/Login.cs
+++ Billeasy_Exam/Login.cs
@@ -22,19 +22,34 @@ namespace Billeasy_Exam
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using(var conn = new SQLiteConnection("Data Source=db/Exam_db.db"))
+            using (var conn = new SQLiteConnection("Data Source=db/Exam_db.db"))
             {
                 conn.Open();
                 var command = conn.CreateCommand();
                 command.CommandText = @"Select * from login where UserName=@username AND Password=@password";
                 command.Parameters.AddWithValue("@username", txtUserName.Text);
                 command.Parameters.AddWithValue("@password", txtPassword.Text);
-                command.ExecuteNonQuery();
+                int result = 0;
+                result = command.ExecuteNonQuery();
                 conn.Close();
+
                 MessageBox.Show("User Logged in successfully!");
                 Form1 form = new Form1();
                 form.Show();
                 this.Hide();
+
+                //string a = "abcd";
+                //string b = "abcd";
+                //if(a==txtUserName.Text && b == txtPassword.Text)
+                //{
+                //    MessageBox.Show("User Logged in successfully!");
+                //    form.Show();
+                //    this.Hide();
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Invalid User");
+                //}
             }
 
         }
